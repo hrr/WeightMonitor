37ca7c78cd2a482d5e896fb36a3e249e75321c7d
hrr <jia@qiu.io>
2017-04-26 17:23:31 +0200

    Added react and bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426142504) do
+ActiveRecord::Schema.define(version: 20170426143119) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "weights", force: :cascade do |t|
+    t.float    "entry"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_weights_on_user_id"
   end
 
 end
