ac0911965f2f9de069377e523130124d4479fb61
hrr <jia@qiu.io>
2017-04-27 12:27:37 +0200

    Limit use of user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :weights, only: [:create, :destroy, :update]
-  resources :users
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
